b5fa6c978d02887095df83c7b146aca5b092b52c
Pablo-Valverde <73491500+Pablo-Valverde@users.noreply.github.com>
2022-01-16 01:58:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["discord", "pillow"]
+requirements = ["discord", "pillow", "Pablo-Valverde/pydiscord"]
 
 setup(
     name="felaciano",
