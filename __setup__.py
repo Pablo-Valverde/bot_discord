fdfb0ec8117d436b4e92c6cbe958fc22d29aafe1
pavalso <73491500+Pablo-Valverde@users.noreply.github.com>
2022-01-16 01:49:41 +0100

    Update __setup__.py

---
+++ __setup__.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["discord", "git+https://github.com/Pablo-Valverde/pydiscord.git", "PIL"]
+
+setup(
+    name="felaciano",
+    version="1.0.0",
+    author="Pablo Valverde",
+    author_email="pabludo8cho@gmail.com",
+    description="Felaciano el makina",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pablo-Valverde/bot_discord",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9.5",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
