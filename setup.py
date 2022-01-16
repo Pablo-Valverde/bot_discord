579620bd90c6759a30a9d7d7ebd2a283d7055d2b
Pablo-Valverde <73491500+Pablo-Valverde@users.noreply.github.com>
2022-01-16 02:00:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["discord", "pillow", "Pablo-Valverde/pydiscord"]
+requirements = ["discord", "pillow", "gh repo clone Pablo-Valverde/pydiscord"]
 
 setup(
     name="felaciano",
